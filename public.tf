7706e697e6129eaecc71c51a30d0523856dc81f6
root <root@DESKTOP-OHN0TFT.>
2024-11-21 02:11:21 -0300

    feat: privatesubnets andvariables

--- public.tf
+++ public.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "eks_subnet_pulic_1a" {
+resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "${data.aws_region.current.name}a"
@@ -7,13 +7,13 @@ resource "aws_subnet" "eks_subnet_pulic_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "terraform-subnet-public-1a",
+      Name                     = "${var.project_name}-subnet-public-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
 
 }
-resource "aws_subnet" "eks_subnet_pulic_1b" {
+resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
@@ -22,7 +22,7 @@ resource "aws_subnet" "eks_subnet_pulic_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "terraform-subnet-public-1b",
+      Name                     = "${var.project_name}-subnet-public-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
