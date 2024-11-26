09d1d71ff3bca3b9ec5a41745c81b450047aaa87
root <root@DESKTOP-OHN0TFT.>
2024-11-26 13:54:44 -0300

    feat: aws load balancer controller module

--- modules/managed-node-group/variables.tf
+++ modules/managed-node-group/variables.tf
@@ -9,16 +9,16 @@ variable "tags" {
 }
 
 variable "cluster_name" {
-    type = string
-    description = "EKS cluster name to create MNG" 
+  type        = string
+  description = "EKS cluster name to create MNG"
 }
 
 variable "subnet_private_1a" {
-    type = string
-    description = "Subnet ID from AZ 1a" 
+  type        = string
+  description = "Subnet ID from AZ 1a"
 }
 
 variable "subnet_private_1b" {
-    type = string
-    description = "Subnet ID from AZ 1b" 
+  type        = string
+  description = "Subnet ID from AZ 1b"
 }
